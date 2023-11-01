a5c2f6650e079dbb11f452b52161e96f7fd47831
janeezy <janeezyofficial@gmail.com>
2023-11-01 09:23:33 +0000

    setting up component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import ShopIcon from '@mui/icons-material/Shop';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header__logo'></div>
+      <ShopIcon className='header__logoImage' fontSize='large' />
+      <h2 className='header__logoTitle'>Jdshop</h2>
+
+      <div className="header__search">
+      <input type="text" className='header__searchInput' />
+        < SearchIcon className='header__searchIcon' />
+        </div>
+
+
+      <div className="header__nav"></div>
+
+      <div className="nav__item"></div>
+      <span className="nav__itemlineone"> Hello Guest</span>
+      <span className="nav__itemlineone"> Sign in</span>
+
+
+      <div className="nav__item"></div>
+      <span className="nav__itemlineone"> Your</span>
+      <span className="nav__itemlineone"> Shop</span>
+
+      <div className="nav__item"></div>
+      <ShoppingBasketIcon fontSize='large'/>
+      <span className="nav__itemlineone"> 0 </span>
     </div>
+
   );
 }
 
