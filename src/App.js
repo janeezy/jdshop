97835992b56da49582ebdfeccd7d6dda2b3f1b36
janeezy <janeezyofficial@gmail.com>
2023-11-03 14:49:27 +0000

    added styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import ShopIcon from '@mui/icons-material/Shop';
-import SearchIcon from '@mui/icons-material/Search';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <div className='header__logo'></div>
-      <ShopIcon className='header__logoImage' fontSize='large' />
-      <h2 className='header__logoTitle'>Jdshop</h2>
-
-      <div className="header__search">
-      <input type="text" className='header__searchInput' />
-        < SearchIcon className='header__searchIcon' />
-        </div>
-
-
-      <div className="header__nav"></div>
-
-      <div className="nav__item"></div>
-      <span className="nav__itemlineone"> Hello Guest</span>
-      <span className="nav__itemlineone"> Sign in</span>
-
-
-      <div className="nav__item"></div>
-      <span className="nav__itemlineone"> Your</span>
-      <span className="nav__itemlineone"> Shop</span>
-
-      <div className="nav__item"></div>
-      <ShoppingBasketIcon fontSize='large'/>
-      <span className="nav__itemlineone"> 0 </span>
+    <div className='App'>
+    <Header/>
     </div>
 
   );
