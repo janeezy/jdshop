aa657a00981e0005a2c50cb3ddf2e3d8c7bde5ca
janeezy <janeezyofficial@gmail.com>
2023-11-11 05:30:16 +0000

    updated props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import Product from './Product';
+import Product from './Product'; // Ensure this is used or remove if it's not needed
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
-
+      <Router>
+        <Header /> {/* Header moved outside Routes if it's common across all routes */}
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/Home' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-
   );
 }
 
